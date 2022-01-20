f5cadfa46c132a30b44cfcecc07337a9bbb8db31
Juliette <97114875+Juliette02@users.noreply.github.com>
2022-01-20 17:14:00 +0100

    20/01/2022

---
+++ Arborescence/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice/js/exo.js
@@ -0,0 +1,15 @@
+var nom = window.prompt("Entrez votre nom : ");
+if(nom==null){
+    alert.prompt("Veuillez entrez votre nom : ");
+    return;
+}
+
+var prenom = windox.prompt("Entrez votre prénom : ");
+if(prenom==null){
+    alert.prompt("Veuillez entrez votre prénom : ")
+}
+
+if (window.confirm("Etes-vous un homme ?"));
+    window.alert("Bonjour Monsieur" + reponse1 + reponse2);
+
+(window.confirm("Etes-vous une femme ?"));
